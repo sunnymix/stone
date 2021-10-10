21abf367208bb9ff1dd719bfd5ddc69839f96813
sunny <sunny@sunnymac.local>
2021-10-11 00:09:29 +0800

    更新 Parser

--- stone05/src/main/java/stone05/ast/parser/Parser.java
+++ stone05/src/main/java/stone05/ast/parser/Parser.java
@@ -11,13 +11,44 @@ import java.util.List;
 public class Parser {
     protected List<Ele> eles;
 
-    protected
+    protected Factory factory;
+
+    public Parser(Class<? extends AstTree> clazz) throws ParseException {
+        reset(clazz);
+    }
+
+    protected Parser(Parser parser) {
+        this.eles = parser.eles;
+        this.factory = parser.factory;
+    }
+
+    public Parser reset(Class<? extends AstTree> clazz) throws ParseException {
+        eles = new ArrayList<>();
+        factory = Factory.forAstList(clazz);
+        return this;
+    }
+
+    public Parser reset() {
+        eles = new ArrayList<>();
+        return this;
+    }
 
     public AstTree parse(Lexer lexer) throws ParseException {
-        ArrayList<AstTree> res = new ArrayList<>();
-        for (Ele ele : eles) {
-            ele.parse(lexer, res);
-        }
+        ArrayList<AstTree> trees = new ArrayList<>();
+        for (Ele ele : eles) ele.parse(lexer, trees);
+        return factory.make(trees);
+    }
 
+    protected boolean match(Lexer lexer) throws ParseException {
+        if (eles.isEmpty()) return true;
+        else return eles.get(0).match(lexer);
+    }
+
+    public static Parser rule(Class<? extends AstTree> clazz) throws ParseException {
+        return new Parser(clazz);
+    }
+
+    public static Parser rule() throws ParseException {
+        return rule(null);
     }
 }
